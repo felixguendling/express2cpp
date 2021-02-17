66252a47fabce0f2f76574d57ddc16a1d0b27b84
Steffen <steffen.heger@gmail.com>
2021-02-17 19:19:36 +0100

    Add get_entity method to model returning non-const entities. Add add_entity method in order to enable insertion of new or modified entities.

--- step/include/step/model.h
+++ step/include/step/model.h
@@ -14,10 +14,23 @@ struct root_entity;
 struct model {
   template <typename T>
   T const& get_entity(step::id_t const& id) const {
+    return const_cast<model*>(this)->get_entity<T>(id);  // NOLINT
+  }
+
+  template <typename T>
+  T& get_entity(step::id_t const& id) {
     utl::verify(id_to_entity_.size() > id.id_, "invalid id");
-    auto const* const entity = dynamic_cast<T*>(id_to_entity_[id.id_]);
+    auto* const entity = dynamic_cast<T*>(id_to_entity_[id.id_]);
     utl::verify(entity != nullptr, "bad cast");
     return *entity;
+  }
+
+  template <typename T>
+  T& add_entity() {
+    auto const e = entity_mem_.emplace_back(std::make_unique<T>()).get();
+    e->id_ = id_to_entity_.size();
+    id_to_entity_.push_back(e);
+    return *static_cast<T*>(e);
   }
 
   std::vector<root_entity*> id_to_entity_;
